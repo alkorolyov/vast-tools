f62399604b97fe3e5e860dd5e1cd73159364f934
Alexander Korolyov <49795502+alkorolyov@users.noreply.github.com>
2024-08-16 07:00:42 +0200

    Update fail2ban.sh

--- fail2ban.sh
+++ fail2ban.sh
@@ -3,10 +3,12 @@ sudo tee /etc/fail2ban/jail.local > /dev/null <<EOL
 [DEFAULT]
 maxretry = 3
 findtime = 5m
+bantime.increment = true
 
 [sshd]
 bantime = 1h
 enabled = true
+
 EOL
 sudo sed -i '0,/port\s*=\s*ssh/s//port    = 22222/' /etc/fail2ban/jail.conf
 sudo systemctl restart fail2ban
