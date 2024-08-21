0bf0655f50c85006da72e0af9ce58b6c1d58c3dc
Alexander Korolyov <49795502+alkorolyov@users.noreply.github.com>
2024-08-21 14:31:27 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,5 @@
 wget https://s3.amazonaws.com/vast.ai/uninstall.py
-sudo python uninstall.py
+sudo python3 uninstall.py
 sudo userdel vastai_kaalia
 sudo rm -rf /var/lib/vastai_kaalia
 sudo rm /etc/systemd/system/vastai_bouncer.service
